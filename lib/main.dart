f2f210d905bbb9e8e9977c453180c1bdbaecdd7c
a-home <solardayz@naver.com>
2021-09-15 22:48:52 +0900

    앱바의 구현 중 - drawer 구현

--- lib/main.dart
+++ lib/main.dart
@@ -22,8 +22,44 @@ class MyPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: Colors.blue[200],
         title: Text('My First App'),
+        centerTitle: true,
+        elevation: 0.0,
+        backgroundColor: Colors.blue[200],
+        // leading: IconButton( drawer 적용을 위해 주석 처리
+        //   icon: Icon(Icons.menu),
+        //   onPressed: () {
+        //     print('menu click');
+        //   },
+        // ),
+        actions: [
+          IconButton(
+            icon: Icon(Icons.search),
+            onPressed: () {
+              print('shopping_bag click');
+            },
+          ),
+        ],
+      ),
+      drawer: Drawer(
+        child: ListView(
+          padding: EdgeInsets.zero, //맨위로 올라감
+          children: [
+            UserAccountsDrawerHeader(
+              accountName: Text('accountName'),
+              accountEmail: Text('accountEmail'),
+              onDetailsPressed: () {
+                print('onDetailsPressed click');
+              },
+              decoration: BoxDecoration(
+                  color: Colors.blue[200],
+                  borderRadius: BorderRadius.only(
+                    bottomLeft: Radius.circular(40.0),
+                    bottomRight: Radius.circular(40.0),
+                  )),
+            )
+          ],
+        ),
       ),
       body: Text('Hello'),
     );
