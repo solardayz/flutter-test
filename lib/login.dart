87b695ae289f1c60ff3007970beca93de3fca540
solardayz <solardayz@naver.com>
2021-09-27 22:17:02 +0900

    controller 구현 및 snackbar 구현을 위한 builder 위젯 추가

--- lib/login.dart
+++ lib/login.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:myapp/dashboard.dart';
 
@@ -9,6 +10,9 @@ class Login extends StatefulWidget {
 }
 
 class _LoginState extends State<Login> {
+  TextEditingController controllerId = TextEditingController();
+  TextEditingController controllerPw = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -17,74 +21,100 @@ class _LoginState extends State<Login> {
           backgroundColor: Colors.blueAccent,
           centerTitle: true,
         ),
-        body: SingleChildScrollView(
-          child: Column(
-            children: [
-              Padding(
-                padding: EdgeInsets.only(top: 50.0),
-              ),
-              Center(
-                child: Text(
-                  '귀뚜라미 냉방 결재 시스템',
-                  style: TextStyle(
-                      fontSize: 20.0,
-                      fontWeight: FontWeight.bold,
-                      color: Colors.blue[300]),
-                ),
-              ),
-              SizedBox(
-                height: 40.0,
-              ),
-              Form(
-                child: Theme(
-                    data: ThemeData(
-                      primaryColor: Colors.blue,
-                      inputDecorationTheme: InputDecorationTheme(
-                          labelStyle:
-                              TextStyle(color: Colors.blue, fontSize: 15.0)),
+        body: Builder(
+          builder:(context) {
+            return SingleChildScrollView(
+              child: Column(
+                children: [
+                  Padding(
+                    padding: EdgeInsets.only(top: 50.0),
+                  ),
+                  Center(
+                    child: Text(
+                      '귀뚜라미 냉방 결재 시스템',
+                      style: TextStyle(
+                          fontSize: 20.0,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.blue[300]),
                     ),
-                    child: Container(
-                      padding: EdgeInsets.all(40.0),
-                      child: Column(
-                        children: [
-                          TextField(
-                            decoration: InputDecoration(labelText: 'Enter ID'),
-                            keyboardType: TextInputType.text,
-                          ),
-                          TextField(
-                            decoration:
-                                InputDecoration(labelText: 'Enter Password'),
-                            keyboardType: TextInputType.text,
-                            obscureText: true,
-                          ),
-                          SizedBox(
-                            height: 30.0,
-                          ),
-                          ButtonTheme(
-                            child: ElevatedButton(
-                              onPressed: () {
-                                print('login');
-                                Navigator.push(
-                                  context,
-                                  MaterialPageRoute(
-                                      builder: (context) => Dashboard()),
-                                );
-                              },
-                              child: Text(
-                                ' Login ',
-                                style: TextStyle(
-                                    fontSize: 20.0,
-                                    fontWeight: FontWeight.bold,
-                                    wordSpacing: 50.0),
+                  ),
+                  SizedBox(
+                    height: 40.0,
+                  ),
+                  Form(
+                    child: Theme(
+                        data: ThemeData(
+                          primaryColor: Colors.blue,
+                          inputDecorationTheme: InputDecorationTheme(
+                              labelStyle:
+                              TextStyle(color: Colors.blue, fontSize: 15.0)),
+                        ),
+                        child: Container(
+                          padding: EdgeInsets.all(40.0),
+                          child: Column(
+                            children: [
+                              TextField(
+                                controller: controllerId,
+                                decoration: InputDecoration(
+                                    labelText: 'Enter ID'),
+                                keyboardType: TextInputType.text,
                               ),
-                            ),
+                              TextField(
+                                controller: controllerPw,
+                                decoration:
+                                InputDecoration(labelText: 'Enter Password'),
+                                keyboardType: TextInputType.text,
+                                obscureText: true,
+                              ),
+                              SizedBox(
+                                height: 30.0,
+                              ),
+                              ButtonTheme(
+                                child: ElevatedButton(
+                                  onPressed: () {
+                                    print('login');
+                                    if (controllerId == 'jhc' &&
+                                        controllerPw == 'A') {
+                                      Navigator.push(
+                                        context,
+                                        MaterialPageRoute(
+                                            builder: (context) => Dashboard()),
+                                      );
+                                    } else {
+                                      // 스낵바 구현할 것
+                                      showSnackBar(BuildContext context);
+                                    }
+                                  },
+                                  child: Text(
+                                    ' Login ',
+                                    style: TextStyle(
+                                        fontSize: 20.0,
+                                        fontWeight: FontWeight.bold,
+                                        wordSpacing: 50.0),
+                                  ),
+                                ),
+                              ),
+                            ],
                           ),
-                        ],
-                      ),
-                    )),
+                        )),
+                  ),
+                ],
               ),
-            ],
-          ),
-        ));
+            );
+          },
+        ),
+    );
   }
+}
+
+void showSnackBar(BuildContext context){
+  Scaffold.of(context).showSnackBar(
+    SnackBar(content:
+      Text('아이디 또는 패스워드가 틀립니다.',
+        textAlign: TextAlign.center,
+      ),
+      duration: Duration(seconds: 2),
+      backgroundColor: Colors.blue,
+    )
+  );
 }
